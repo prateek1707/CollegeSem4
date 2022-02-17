aab8a7b48106b9952cbdac0735ae2c63b6c2d029
prateek1707 <70459277+prateek1707@users.noreply.github.com>
2022-02-17 21:52:12 +0530

    init commit

---
+++ src/Lab6/GreaterThan.java
@@ -0,0 +1,13 @@
+package Lab6;
+
+public class GreaterThan
+{
+    public static void main(String[] args) {
+        int arr[]={3,8,31,2,19,91,0};
+        for(int i:arr)
+        {
+            if(i>10)
+                System.out.println(i);
+        }
+    }
+}
